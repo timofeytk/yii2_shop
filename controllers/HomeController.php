cca3b45dffb0c481582b057d227ab02d614977dd
timofeytk <timofeytk@yandex.ru>
2021-01-25 22:35:15 +0600

    yii.loc init

---
+++ controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace app\controllers;
+
+
+class HomeController extends AppController
+{
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
